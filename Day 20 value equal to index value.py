b5c6e19bb6f7f1ff1aaaa98b315edc639f5d9299
AprajitaChhawi <achhawip@gmail.com>
2021-03-20 23:21:32 +0530

    Create Day 20 value equal to index value.py

---
+++ Day 20 value equal to index value.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+class Solution:
+
+	def valueEqualToIndex(self,arr, n):
+	    ans=[]
+	    for i in range(n):
+	        if arr[i]==i+1:
+	            ans.append(i+1)
+	    return ans
+		# code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.valueEqualToIndex(arr, n)
+        if len(ans) == 0:
+            print("Not Found")
+        else:
+            for x in ans:
+                print(x, end=" ")
+            print()
+        tc -= 1
+
+# } Driver Code Ends
