3b53461b655d0cbe28b7a458b3ec7d79dbe2911b
AprajitaChhawi <achhawip@gmail.com>
2021-03-20 23:20:48 +0530

    Create Day 18 least common ansector of two nodes.py

---
+++ Day 18 least common ansector of two nodes.py
@@ -0,0 +1,124 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+'''
+# If LCA exist, return reference to it. If
+# If both n1 and n2 are not present, 
+# rturn None. Else if left subtree contains any 
+#  of them return pointer to left.
+class Solution:
+    def lca(self,root, n1, n2):
+        if root==None:
+            return None
+        if root.data==n1 or root.data==n2:
+            return root
+        ob=Solution()
+        left=ob.lca(root.left,n1,n2)
+        right=ob.lca(root.right,n1,n2)
+        if left==None and right==None:
+            return None
+        if left and right:
+            return root
+        else:
+            if left:
+                return left
+            else:
+                return right
+                return left
+        # Code here
+
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+#Contributed by Sudarshan Sharma
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        a,b=list(map(int,input().split()))
+        s=input()
+        root=buildTree(s)
+        k=Solution().lca(root,a,b)
+        print(k.data)
+
+
+
+# } Driver Code Ends
