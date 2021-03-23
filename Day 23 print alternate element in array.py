14a8a62a19abe63de21a0a6ad84c39ee14c1bc70
AprajitaChhawi <achhawip@gmail.com>
2021-03-23 23:13:50 +0530

    Create Day 23 print alternate element in array.py

---
+++ Day 23 print alternate element in array.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+# arr is the array
+# n is the number of elements in array
+def printAl(arr,n):
+    for i in range(0,n,2):
+        print(arr[i],end=" ")
+    # your code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        printAl(arr,n)
+        print()
+# } Driver Code Ends
